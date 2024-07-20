5789b4873c1d0cf6e783694a226d9c7d1558d1ec
elkiki99 <brossani23@gmail.com>
2024-07-20 20:12:55 -0300

    primary nav-bar for small screens

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,7 +2,7 @@
 
 @php
 $classes = ($active ?? false)
-    ? 'inline-flex items-center px-2 border-indigo-400 dark:border-indigo-600 text-sm font-semibold leading-5 text-black dark:text-white focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
+    ? 'inline-flex items-center px-2 border-indigo-400 hover:blur-xs dark:border-indigo-600 text-sm leading-5 text-black dark:text-white focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
     : 'inline-flex items-center px-1 border-transparent text-sm font-medium leading-5 text-black dark:text-gray-100 hover:blur-xs hover:border-gray-300 focus:outline-none transition duration-150 ease-in-out';
 @endphp
 
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,18 +1,18 @@
 <nav x-data="{ open: false }" class="fixed top-0 z-50 w-full px-3 mx-auto text-sm font-medium text-center bg-transparent rounded-none sm:rounded-full sm:w-auto sm:top-10 backdrop-filter backdrop-blur-3xl sm:flex dark:text-zinc-200 dark:ring-white/10">
     <!-- Primary Navigation Menu -->
-    <div class="flex items-center justify-between h-16 px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div class="flex items-center justify-between h-12 px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <!-- Logo -->
         <div class="flex items-center">
             <a href="{{ route('welcome') }}">
-                <x-application-logo class="block w-auto h-10 text-black fill-current dark:text-gray-200" />
+                <x-application-logo class="block w-auto text-black fill-current size-8 dark:text-gray-200" />
             </a>
         </div>
 
         <!-- Hamburguer for mobile -->
         <div class="sm:hidden">
-            <button @click="open = !open" class="inline-flex items-center justify-center p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white dark:focus:ring-gray-800">
-                <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                    <path :class="{'hidden': open, 'block': !open }" class="block" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+            <button @click="open = !open" class="inline-flex items-center justify-center p-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path :class="{'hidden': open, 'block': !open }" class="block" stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
                     <path :class="{'hidden': !open, 'block': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
                 </svg>
             </button>
@@ -44,7 +44,7 @@
                     </x-nav-link>
                 </form>
             @endif
-            <x-nav-link wire:navigate :href="route('newsletter')" class="inline-flex items-center px-4 py-2 text-white bg-gray-900 rounded-2xl hover:blur-xs dark:text-black dark:hover:text-black dark:focus:text-black">
+            <x-nav-link wire:navigate :href="route('newsletter')" class="inline-flex items-center px-3 py-1 text-white bg-gray-900 rounded-2xl hover:blur-xs dark:bg-gray-100 dark:text-black hover:text-white hover:border-gray-300 dark:hover:text-black focus:text-white focus:border-gray-300 dark:focus:text-black">
                 <div class="flex items-center">
                     {{ __('Newsletter') }}
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-2">
@@ -54,8 +54,6 @@
             </x-nav-link>
         </div>
     </div>
-
-
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': !open}" class="hidden sm:hidden">
@@ -84,32 +82,15 @@
                     </x-responsive-nav-link>
                 </form>
             @endif
-            <x-responsive-nav-link wire:navigate :href="route('newsletter')" class="inline-flex items-center w-full px-4 py-2 text-white bg-gray-900 rounded-2xl hover:blur-xs dark:bg-gray-100 dark:text-black hover:text-white hover:border-gray-300 dark:hover:text-black focus:text-white focus:border-gray-300 dark:focus:text-black">
-                <div class="flex items-center">
-                    {{ __('Newsletter') }}
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-                    </svg>
-                </div>
-            </x-responsive-nav-link>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
-            <div class="px-4">
-                <div class="text-base font-medium text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                <div class="text-sm font-medium text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-            <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
+            <div class="flex justify-start">
+                <x-responsive-nav-link wire:navigate :href="route('newsletter')" class="inline-flex items-center px-3 py-1 text-white bg-gray-900 rounded-2xl hover:blur-xs dark:bg-gray-100 dark:text-black hover:text-white hover:border-gray-300 dark:hover:text-black focus:text-white focus:border-gray-300 dark:focus:text-black" style="width: auto;">
+                    <div class="flex items-center">
+                        {{ __('Newsletter') }}
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                        </svg>
+                    </div>
                 </x-responsive-nav-link>
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
             </div>
         </div>
     </div>
