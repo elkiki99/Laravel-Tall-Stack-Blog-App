3b30d89c72e2788981c83d94ada37ece187841c3
elkiki99 <brossani23@gmail.com>
2024-07-29 17:27:34 -0300

    search bar for categories

--- app/Livewire/Categories/ShowCategories.php
+++ app/Livewire/Categories/ShowCategories.php
@@ -4,15 +4,25 @@ namespace App\Livewire\Categories;
 
 use Livewire\Component;
 use App\Models\Category;
+use Livewire\WithPagination;
+use Illuminate\Database\Eloquent\Builder;
 
 class ShowCategories extends Component
-{   
+{
+    use WithPagination;
+
+    public string $searchCategory = '';
+
     public function render()
     {
-        $categories = Category::all();
+        $categories = Category::query()
+            ->when($this->searchCategory !== '', function (Builder $query) {
+                $query->where('name', 'like', '%' . $this->searchCategory . '%');
+            })
+            ->paginate(20);
 
         return view('livewire.categories.show-categories', [
-            'categories' => $categories
+            'categories' => $categories,
         ]);
     }
 }
--- resources/views/livewire/categories/show-categories.blade.php
+++ resources/views/livewire/categories/show-categories.blade.php
@@ -1,5 +1,19 @@
-<div class="mt-5 overflow-x-auto">
+<div class="min-h-screen mt-5 overflow-x-auto">
     @if($categories->count() > 0)
+        <div class="w-full mb-5 md:w-1/2">
+            <form class="flex items-center p-1">
+                <label for="simple-search" class="sr-only">Search</label>
+                <div class="relative w-full">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <input wire:model.live="searchCategory" type="text" id="simple-search" class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search categories..." required="">
+                </div>
+            </form>
+        </div>
+
         <table class="min-w-full bg-white">
             <thead class="bg-gray-100">
                 <tr class="">
@@ -74,6 +88,9 @@
                 @endforeach
             </tbody>
         </table>
+        <div class="justify-end px-1 mt-10">
+            <span>{{ $categories->links() }}</span>
+        </div>
     @else
         <div class="p-6 bg-gray-100 shadow-md rounded-xl">
             <p class="text-lg">No categories created yet. 
