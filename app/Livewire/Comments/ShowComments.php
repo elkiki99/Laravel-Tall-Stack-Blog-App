eb1eadf0c0ad298a2c899719bc6081d2c6ae36f9
elkiki99 <brossani23@gmail.com>
2024-08-06 19:56:56 -0300

    search bar for comments.index

--- app/Livewire/Comments/ShowComments.php
+++ app/Livewire/Comments/ShowComments.php
@@ -4,12 +4,22 @@ namespace App\Livewire\Comments;
 
 use App\Models\Comment;
 use Livewire\Component;
+use Livewire\WithPagination;
+use Illuminate\Database\Eloquent\Builder;
 
 class ShowComments extends Component
 {
+    use WithPagination;
+    
+    public $searchComments = '';
+    
     public function render()
     {
-        $comments = Comment::latest()->paginate(20);
+        $comments = Comment::query()
+            ->when($this->searchComments !== '', function (Builder $query) {
+                $query->where('body', 'like', '%' . $this->searchComments . '%');
+            })
+            ->paginate(20);
         
         return view('livewire.comments.show-comments', [
             'comments' => $comments
