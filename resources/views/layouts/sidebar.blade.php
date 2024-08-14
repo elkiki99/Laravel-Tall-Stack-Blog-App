b8332355301d8273d655aa77f31a5f475fa5e780
elkiki99 <brossani23@gmail.com>
2024-08-14 01:31:31 -0300

    small_sidebar updated

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -115,7 +115,7 @@
                 @mouseover="hoverSection = 'users'"
                 @mouseleave="hoverSection = null" 
                 @click="activeSection = 'users'; hoverSection = null">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="size-7 m-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="m-2 size-7">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                 </svg>
             </div>
@@ -261,7 +261,7 @@
             </a> --}}
         </div>
         <div x-show="hoverSection === 'users' || (hoverSection === null && activeSection === 'users')"
-        class="transition-opacity duration-300">
+            class="transition-opacity duration-300">
             <a wire:navigate href="{{ route('users.index') }}">
                 <div
                     class="justify-between p-2 m-2 transition-transform duration-300 bg-transparent rounded-lg hover:cursor-pointer hover:shadow-lg hover:scale-[1.02] backdrop-filter backdrop-blur-3xl dark:bg-gray-800">
--- resources/views/layouts/small_sidebar.blade.php
+++ resources/views/layouts/small_sidebar.blade.php
@@ -69,10 +69,20 @@
                         </svg>
                     </button>
                 </div>
+                <div class="flex items-center justify-center hover:blur-xs">
+                    <button @click="openMenuIndex = openMenuIndex === 6 ? null : 6"
+                        class="{{str_contains($routeName, 'users') ? 'bg-gray-800 rounded-full sm:rounded-md' : ''}}"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        class="m-2 size-6 sm:size-7 stroke-black sm:stroke-white {{str_contains($routeName, 'users') ? 'stroke-white' : ''}}">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                        </svg>
+                    </button>
+                </div>
             @endif
             @if(auth()->user()->role === 'user')
                 <div class="flex items-center justify-center hover:blur-xs">
-                    <button @click="openMenuIndex = openMenuIndex === 6 ? null : 6"
+                    <button @click="openMenuIndex = openMenuIndex === 7 ? null : 7"
                         class="{{str_contains($routeName, 'subscription') ? 'bg-gray-800 rounded-full sm:rounded-md' : ''}}"
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -82,7 +92,7 @@
                     </button>
                 </div>
                 <div class="flex items-center justify-center hover:blur-xs">
-                    <button @click="openMenuIndex = openMenuIndex === 7 ? null : 7"
+                    <button @click="openMenuIndex = openMenuIndex === 8 ? null : 8"
                         class="{{str_contains($routeName, 'liked') ? 'bg-gray-800 rounded-full sm:rounded-md' : ''}}"
                     >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -90,10 +100,10 @@
                             <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                         </svg>
                     </button>
-                </div>
+                </div>  
             @endif
             <div class="flex items-center justify-center hover:blur-xs">
-                <button @click="openMenuIndex = openMenuIndex === 8 ? null : 8"
+                <button @click="openMenuIndex = openMenuIndex === 9 ? null : 9"
                 class="{{str_contains($routeName, 'profile') ? 'bg-gray-800 rounded-full sm:rounded-md' : ''}}"
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -131,16 +141,20 @@
             <div x-show="openMenuIndex === 5"
                 class="absolute top-0 z-40 w-56 mt-2 bg-white rounded-md shadow-lg -right-60 backdrop-filter backdrop-blur-3xl">
                 <a wire:navigate href="{{ route('comments.index') }}" class="block px-4 py-2 text-sm text-black hover:bg-gray-200">New comments</a>
-            </div>  
+            </div> 
             <div x-show="openMenuIndex === 6"
+                class="absolute top-0 z-40 w-56 mt-2 bg-white rounded-md shadow-lg -right-60 backdrop-filter backdrop-blur-3xl">
+                <a wire:navigate href="{{ route('users.index') }}" class="block px-4 py-2 text-sm text-black hover:bg-gray-200">All users</a>
+            </div>  
+            <div x-show="openMenuIndex === 7"
                 class="absolute top-0 z-40 w-56 mt-2 bg-white rounded-md shadow-lg -right-60 backdrop-filter backdrop-blur-3xl">
                 <a wire:navigate href="#" class="block px-4 py-2 text-sm text-black hover:bg-gray-200">My subscription</a>
             </div>
-            <div x-show="openMenuIndex === 7"
+            <div x-show="openMenuIndex === 8"
                 class="absolute top-0 z-40 w-56 mt-2 bg-white rounded-md shadow-lg -right-60 backdrop-filter backdrop-blur-3xl">
                 <a wire:navigate href="#" class="block px-4 py-2 text-sm text-black hover:bg-gray-200">My likes</a>
             </div>        
-            <div x-show="openMenuIndex === 8"
+            <div x-show="openMenuIndex === 9"
                 class="absolute top-0 z-40 w-56 mt-2 bg-white rounded-md shadow-lg -right-60 backdrop-filter backdrop-blur-3xl">
                 @if(auth()->user()->role === 'author')
                     <a  wire:navigate href="{{ route('users.show', App\Models\User::find(auth()->user()->id)) }}" class="block px-4 py-2 text-sm text-black hover:bg-gray-200">View profile</a>
